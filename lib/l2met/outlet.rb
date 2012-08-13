bc911b92efd67fc7b161d5f2c9f26da274c5ab0a
♠ ace hacker <ryan@heroku.com>
2012-08-13 08:10:08 -0700

    remove until we can abstract

--- lib/l2met/outlet.rb
+++ lib/l2met/outlet.rb
@@ -11,19 +11,6 @@ module L2met
       Scrolls.log({ns: "outlet"}.merge(data), &blk)
     end
 
-    def heartbeat
-      log(fn: __method__) do
-        @measured = Atomic.new(0)
-        Thread.new do
-          loop do
-            n = @measured.swap(0)
-            log(fn: __method__, at: "emit", received: n)
-            sleep(1)
-          end
-        end
-      end
-    end
-
     def handle(data)
       if data.key?("measure")
         if data.key?("elapsed")
@@ -35,7 +22,6 @@ module L2met
           name = [data["app"], data["at"]].compact.join(".")
           Metric.counter(name: name, source: data["source"])
         end
-        @measured.update {|n| n + 1}
       end
     end
 

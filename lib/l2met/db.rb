c85dbe3d2896f1c89045942c7c0b578629080c33
♠ ace hacker <ryan@heroku.com>
2012-08-21 19:01:29 -0700

    debug

--- lib/l2met/db.rb
+++ lib/l2met/db.rb
@@ -20,6 +20,7 @@ module L2met
             data = opts.merge(mkey: mkey, uuid: uuid, value: value)
             log(fn: __method__, at: 'creation', data: data)
             DB[tname].put(data)
+            log(opts: opts)
             if opts[:consumer]
               DB["active-stats"].put(mkey: mkey, consumer: opts[:consumer],
                                       time: Time.now.to_s)

b77429f5f6308620bdfdcca2c5e7718a01a36999
♠ ace hacker <ryan@heroku.com>
2012-08-24 10:00:13 -0700

    force ssl

--- lib/l2met/web.rb
+++ lib/l2met/web.rb
@@ -11,7 +11,7 @@ require 'l2met/consumer'
 
 module L2met
   class Web < Sinatra::Base
-    #use Rack::SslEnforcer
+    use Rack::SslEnforcer
     register Sinatra::GoogleAuth
     set :public_folder, "./public"
     set :views, "./templates"

43f0398dcdf1f0629a63f86ad1c6aae8c9934248
♠ ace hacker <ryan@heroku.com>
2012-09-08 15:32:49 -0700

    add gc

--- Procfile
+++ Procfile
@@ -1,3 +1,4 @@
 web: bundle exec bin/web
 receiver: bundle exec bin/receiver
 dboutlet: bundle exec bin/dboutlet
+gc: bundle exec bin/gc
---
+++ lib/l2met/gc.rb
@@ -0,0 +1,41 @@
+require 'scrolls'
+require 'l2met/heartbeat'
+require 'l2met/db'
+
+module L2met
+  module GC
+    extend self
+    INTERVAL = 60
+
+    def start
+      loop do
+        t = Time.now.to_i
+        #Thread.new {collect(t - (60*60))}
+        collect(t - (60*60))
+        sleep(INTERVAL)
+      end
+    end
+
+    def collect(t)
+      log(fn: __method__, time: Time.at(t)) do
+        DB["active-stats"].each do |item|
+          mkey = item.attributes["mkey"]
+          %w(counters histograms last_vals).each do |tname|
+            log(at: tname)
+            DB[tname].query(hash_value: mkey).each do |i|
+              if i.attributes["time"].to_i < t
+                i.delete
+                Heartbeat.pulse("gc-collect-#{tname}")
+              end
+            end
+          end
+        end
+      end
+    end
+
+    def log(data, &blk)
+      Scrolls.log({ns: "gc"}.merge(data), &blk)
+    end
+
+  end
+end

f9657be52126516c0c5146d62bc23925441d7ea0
♠ ace hacker <ryan@heroku.com>
2012-11-26 13:51:35 -0800

    cut down the verbosity of logging

--- lib/l2met/utils.rb
+++ lib/l2met/utils.rb
@@ -18,9 +18,9 @@ module L2met
       Yajl::Encoder.encode(data)
     end
 
-    def count(val, name)
+    def count(val, name, verbose=false)
       name = Config.app_name + '.' + name
-      log(measure: name, val: val)
+      log(measure: name, val: val) if verbose
       Register.accept(name, val,
         type: "counter",
         source: Config.app_name,
@@ -28,9 +28,9 @@ module L2met
         time: Time.now.to_i)
     end
 
-    def time(elapsed, name)
+    def time(elapsed, name, verbose=false)
       name = Config.app_name + '.' + name
-      log(measure: name, val: elapsed)
+      log(measure: name, val: elapsed) if verbose
       Register.accept(name, Float(elapsed),
         type: "list",
         source: Config.app_name,
@@ -38,11 +38,11 @@ module L2met
         time: Time.now.to_i)
     end
 
-    def measure(name)
+    def measure(name, verbose=false)
       t0 = Time.now
       yield.tap do
-        count(1, name)
-        time(Time.now-t0, name)
+        count(1, name, verbose)
+        time(Time.now-t0, name, verbose)
       end
     end
 

d29a81777c563a63335f35dd6aaef18fbdfc7295
♠ ace hacker <ryan@heroku.com>
2013-01-24 14:29:06 -0800

    format sql

--- sql/functions.sql
+++ sql/functions.sql
@@ -9,15 +9,16 @@ returns table(
 )
 as $$
 select
-  name,
-  source,
-  date_trunc('hour', bucket) + date_part('minute', bucket)::int / $2 * ($2 || 'min')::interval,
-  array_accum(vals)
-from metrics
+	name,
+	source,
+	date_trunc('hour', bucket) + date_part('minute', bucket)::int / $2 * ($2 || 'min')::interval,
+	array_accum(vals)
+from
+	metrics
 where
-  token = $1::uuid
-  and bucket > $3
-  and bucket <= $4
+	token = $1::uuid
+	and bucket > $3
+	and bucket <= $4
 group by 1, 2, 3
 order by 3 desc
 $$ language sql immutable;

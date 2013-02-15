e8f718debe68531efae2fdef004504cddd8c3ca0
♠ ace hacker <ryan@heroku.com>
2013-02-14 23:58:50 -0800

    without purpose

--- utils/utils.go
+++ utils/utils.go
@@ -25,11 +25,6 @@ func init() {
 	}
 }
 
-func Measure(n string) {
-	n = appName + "." + n
-	fmt.Printf("measure=%q\n", n)
-}
-
 func MeasureI(n string, i int64) {
 	n = appName + "." + n
 	fmt.Printf("measure=%q val=%d\n", n, i)

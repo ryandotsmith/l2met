8e0fb62ab6f5a25bbe5f74dd196903e4718ee3a7
♠ ace hacker <ryan@heroku.com>
2013-04-01 23:41:13 +0000

    test bucket id parsing

---
+++ bucket/id_test.go
@@ -0,0 +1,29 @@
+package bucket
+
+import (
+	"testing"
+	"fmt"
+)
+
+func TestParseId(t *testing.T) {
+	expected, err := ParseId("1364858753→1000000000→123→hello→machine")
+	if err != nil {
+		t.FailNow()
+	}
+	if expected.Source != "machine" {
+		fmt.Printf("expected=%d actual=%d\n", "machine", expected.Source)
+		t.FailNow()
+	}
+	if expected.Name != "hello" {
+		fmt.Printf("expected=%d actual=%d\n", "hello", expected.Name)
+		t.FailNow()
+	}
+	if expected.Token != "123" {
+		fmt.Printf("expected=%d actual=%d\n", "123", expected.Token)
+		t.FailNow()
+	}
+	if expected.Resolution != 1000000000 {
+		fmt.Printf("expected=%d actual=%d\n", 1000, expected.Resolution)
+		t.FailNow()
+	}
+}

e19084656e0a6e2c11362c134d1dfd3c0deb99fd
♠ ace hacker <ryan@heroku.com>
2013-04-08 14:57:46 -0700

    just replace @ for now

--- outlet/librato_outlet.go
+++ outlet/librato_outlet.go
@@ -4,13 +4,13 @@ import (
 	"bytes"
 	"encoding/json"
 	"errors"
+	"strings"
 	"fmt"
 	"io/ioutil"
 	"l2met/bucket"
 	"l2met/token"
 	"l2met/utils"
 	"net/http"
-	"net/url"
 	"strconv"
 	"time"
 )
@@ -207,11 +207,11 @@ func (l *LibratoOutlet) post(tok *token.Token, body *bytes.Buffer) error {
 		return err
 	}
 	req.Header.Add("Content-Type", "application/json")
-	escapedUser, err := url.QueryUnescape(tok.User)
+	tok.User = strings.Replace(tok.User, "@", "%40", 1)
 	if err != nil {
 		return err
 	}
-	req.SetBasicAuth(escapedUser, tok.Pass)
+	req.SetBasicAuth(tok.User, tok.Pass)
 
 	resp, err := http.DefaultClient.Do(req)
 	if err != nil {
